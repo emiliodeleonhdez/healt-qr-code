33d587c50ad5d6e628f8b933f85d8e640665cfc2
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2025-08-21 12:09:02 -0600

    Add pritter and tailwind plugin

--- src/app/components/molecules/Profile/Conditions.tsx
+++ src/app/components/molecules/Profile/Conditions.tsx
@@ -1,21 +1,18 @@
-import { FileText } from "lucide-react";
-import React from "react";
+import { FileText } from 'lucide-react';
+import React from 'react';
 
 const Conditions = () => {
   return (
-    <article
-      className="flex flex-col gap-2 rounded-2xl border border-violet-300 bg-white shadow-md p-4 
-  w-full max-w-md md:max-w-lg lg:max-w-2xl"
-    >
+    <article className="flex w-full max-w-md flex-col gap-2 rounded-2xl border border-violet-300 bg-white p-4 shadow-md md:max-w-lg lg:max-w-2xl">
       <section className="flex gap-2">
         <FileText className="h-6 w-6 text-violet-600" />
-        <h2 className="font-bold text-violet-600 text xl">
+        <h2 className="text xl font-bold text-violet-600">
           Enfermedades Existentes
         </h2>
       </section>
       <section className="flex flex-wrap gap-2">
-        <p className="p-2 rounded-xl border border-violet-300">Diabetes</p>
-        <p className="p-2 rounded-xl border border-violet-300">Hipertensión</p>
+        <p className="rounded-xl border border-violet-300 p-2">Diabetes</p>
+        <p className="rounded-xl border border-violet-300 p-2">Hipertensión</p>
       </section>
     </article>
   );
