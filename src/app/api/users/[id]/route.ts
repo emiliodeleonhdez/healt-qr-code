f25ff0d3b9d54740940a3c52fb42f74c20cba7d0
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2025-08-25 11:00:07 -0600

    Fix routes with userdata schema

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,8 +1,7 @@
-import { NextResponse } from "next/server";
-import User, { UserData } from "../../../models/Users"; // Importar el modelo de Usuario
-import connectDb from "@/app/lib/mongoose";
+import { NextResponse } from 'next/server';
+import User, { UserData } from '../../../models/Users';
+import connectDb from '@/app/lib/mongoose';
 
-// Obtener usuario por ID
 export async function GET(
   req: Request,
   { params }: { params: Promise<{ id: string }> }
@@ -15,33 +14,31 @@ export async function GET(
     const user = await User.findById(id);
 
     if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
     }
 
     return NextResponse.json({ user }, { status: 200 });
   } catch (error) {
-    console.error("Error fetching user:", error);
-    return NextResponse.json({ error: "Failed to get user" }, { status: 500 });
+    console.error('Error fetching user:', error);
+    return NextResponse.json({ error: 'Failed to get user' }, { status: 500 });
   }
 }
 
-// Actualizar usuario por ID
 export async function PUT(
   request: Request,
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   const {
-    name,
-    lastName,
-    birthDate,
-    phoneNumber,
+    fullName,
+    dateOfBirth,
     bloodType,
     allergies,
-    preExistingConditions,
     currentTreatment,
+    existingConditions,
+    additionalInfo,
     emergencyContacts,
-    medicalInsurancePolicy, // Póliza de seguro médico
+    insurance,
   }: UserData = await request.json();
 
   try {
@@ -51,31 +48,30 @@ export async function PUT(
     const updatedUser = await User.findByIdAndUpdate(
       id,
       {
-        name,
-        lastName,
-        birthDate,
-        phoneNumber,
+        fullName,
+        dateOfBirth,
         bloodType,
         allergies,
-        preExistingConditions,
         currentTreatment,
+        existingConditions,
+        additionalInfo,
         emergencyContacts,
-        medicalInsurancePolicy, // Actualizar póliza de seguro médico
+        insurance,
       },
       { new: true } // Devuelve el documento actualizado
     );
 
     if (!updatedUser) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
     }
 
     return NextResponse.json(
-      { message: "User updated successfully", user: updatedUser },
+      { message: 'User updated successfully', user: updatedUser },
       { status: 200 }
     );
   } catch (error) {
-    console.error("Error updating user:", error);
-    return NextResponse.json({ error: "Error updating user" }, { status: 500 });
+    console.error('Error updating user:', error);
+    return NextResponse.json({ error: 'Error updating user' }, { status: 500 });
   }
 }
 
@@ -91,14 +87,14 @@ export async function DELETE(
 
     const userDelete = await User.findByIdAndDelete(id);
     if (!userDelete) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
     }
     return NextResponse.json(
-      { message: "User deleted", user: userDelete },
+      { message: 'User deleted', user: userDelete },
       { status: 200 }
     );
   } catch (error) {
-    console.error("Error deleting user:", error);
-    return NextResponse.json({ error: "Error deleting user" }, { status: 500 });
+    console.error('Error deleting user:', error);
+    return NextResponse.json({ error: 'Error deleting user' }, { status: 500 });
   }
 }
